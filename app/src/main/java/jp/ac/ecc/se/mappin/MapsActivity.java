f537a6544ffe445e92aeb7f332bf3b0090102a5b
nezimakidori <karasawa2220256@gmail.com>
2023-11-02 14:50:20 +0900

    Mapsactivityの追加　googlemapapiの導入

---
+++ app/src/main/java/jp/ac/ecc/se/mappin/MapsActivity.java
@@ -0,0 +1,121 @@
+package jp.ac.ecc.se.mappin;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+        ,GoogleMap.OnPoiClickListener {
+    private final int FINE_PERMISSION_CODE = 1;
+    private GoogleMap mMap;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    //位置情報
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    currentLocation =location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MapsActivity.this);
+                }
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == FINE_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }else {
+                Toast.makeText(this,"位置情報の許可が拒否されました 許可してください",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        //現在の位置情報の取得
+        if (currentLocation != null) {
+            LatLng sydney = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(sydney).title("現在地"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap.setOnPoiClickListener(this);
+            //ここでカメラの位置を調整できる
+            CameraUpdate locationUpdate = CameraUpdateFactory.newLatLngZoom(sydney, 15.0f);
+            mMap.moveCamera(locationUpdate);
+
+        } else {
+            // currentLocationがnullの場合の処理
+        }
+
+
+
+//    //        ここでマーカーの色を自由に決めれる
+//            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+//            mMap.addMarker(options);
+    }
+
+
+
+
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        Toast.makeText(this, "場所：" +
+                        poi.name,
+//                        + "\nID:" + poi.placeId +
+//                        "\nLatitude:" + poi.latLng.latitude +
+//                        " Longitude:" + poi.latLng.longitude,
+                Toast.LENGTH_SHORT).show();
+    }
+}
+
+
+
+
+
--- app/src/main/java/jp/ac/ecc/se/mappin/PostPreparation.java
+++ app/src/main/java/jp/ac/ecc/se/mappin/PostPreparation.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class PostPreparation extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_preparation);
