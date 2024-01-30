68b855871435ab8507ab1f4338287f5d4d0321e0
2220276 <2220276@ecc.ac.jp>
2024-01-30 09:26:55 +0900

    preparationのサーバーに画像のアップロード完了

--- app/src/main/java/jp/ac/ecc/se/mappin/Account.java
+++ app/src/main/java/jp/ac/ecc/se/mappin/Account.java
@@ -3,9 +3,12 @@ package jp.ac.ecc.se.mappin;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Account extends AppCompatActivity {
 
@@ -15,6 +18,30 @@ public class Account extends AppCompatActivity {
 
         //画面部品の宣言
         ImageView settingButton = findViewById(R.id.setting_image);
+        ImageView icon_image = findViewById(R.id.icon_image);
+        EditText name_text =findViewById(R.id.name_text);
+        TextView userid_text = findViewById(R.id.userid_text);
+        EditText total_text = findViewById(R.id.total_text);
+        ImageView good_image = findViewById(R.id.good_image);
+        ImageView heart_image = findViewById(R.id.heart_image);
+        ImageView smile_image = findViewById(R.id.smile_image);
+        TextView good = findViewById(R.id.number_good);
+        TextView heart = findViewById(R.id.number_heart);
+        TextView smile = findViewById(R.id.number_smile);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        RecyclerView.Adapter adapter = new SampleAdapter(createCellDate());
+//
+//        recyclerView.setAdapter(adapter);
+//
+//        DividerItemDecoration decorator = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+//        recyclerView.addItemDecoration(decorator);
+
+
 
         SharedPreferences sharedPreferences;
 
